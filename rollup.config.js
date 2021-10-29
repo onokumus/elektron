e26bd7a2a4dc5adfefef24145e9aa0b9019c815e
Osman Nuri Okumus <onokmus@gmail.com>
2021-10-29 21:48:26 +0300

    update banner

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,15 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
-const banner = `/*!
-* ${pkg.name} - v${pkg.version}
-* ${pkg.description}
-* https://github.com/onokumus/${pkg.name}#readme
-*
-* Made by Osman Nuri Okumus <onokumus@gmail.com> (https://github.com/onokumus)
-* Under MIT License
-*/`;
-
+const banner = `/*! ${pkg.name} v${pkg.version} | MIT License | https://github.com/onokumus/${pkg.name} */`;
 
 export default [
   {
     input: 'js/elektron.js',
-    external: ['jquery'],
     output: [
       {
         name: 'elektron',
         banner,
-        globals: {
-          jquery: 'jQuery',
-        },
         file: pkg.browser,
         format: 'umd',
         sourcemap: true,
