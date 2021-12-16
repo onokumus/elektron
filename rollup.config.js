5e7476ead9d5e63d3340b44202de990c0a1f1323
Osman Nuri Okumus <onokumus@gmail.com>
2021-12-16 21:54:06 +0300

    add dev mode

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 const banner = `/*! ${pkg.name} v${pkg.version} | MIT License | https://github.com/onokumus/${pkg.name} */`;
+const production = !process.env.ROLLUP_WATCH;
 
 export default [
   {
@@ -10,7 +11,7 @@ export default [
       {
         name: 'elektron',
         banner,
-        file: pkg.browser,
+        file: production ? pkg.browser : 'docs/assets/js/elektron.js',
         format: 'umd',
         sourcemap: true,
       },
