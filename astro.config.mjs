099093945824d63477819104efead10efa255ee7
Osman Nuri Okumuş <onokumus@gmail.com>
2023-05-04 09:39:42 +0300

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    root: './site',
-    srcDir: './site/src',
-    publicDir: './site/public',
-    build:{
-        format: 'file'
-    },
-    site: 'https://onokumus.github.io',
-    base: '/elektron',
-    markdown: {
-        syntaxHighlight:'prism'
-    }
+  root: './site',
+  srcDir: './site/src',
+  publicDir: './site/public',
+  site: 'https://onokumus.github.io',
+  base: '/elektron/',
+  markdown: {
+    syntaxHighlight: 'prism'
+  }
 });
